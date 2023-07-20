9ab4e7a85ca19d936b3b4643dd909747a37636ef
Nevo Swissa <96447671+NevoSwissa@users.noreply.github.com>
2023-07-20 17:56:53 +0300

    Update config.lua

--- CL-Reports/config.lua
+++ CL-Reports/config.lua
@@ -8,5 +8,5 @@ Config.Timeout = {
 Config.Discord = {
     Enable = true, -- Set to false to disable the use of discord logs
     Image = "https://cdn.discordapp.com/attachments/967914093396774942/1125782793570488360/CLOUD_LOGO.png", -- The image used in the discord logs
-    Webhook = "https://discord.com/api/webhooks/1010869631692591206/AIUzNU4Hl_2wA9K_hU__I3oYrNqKptTMZeQ_gZA3AlhAj6nNs_w6idysZ-n5pzIUed8R", -- The discord webhook where the logs will be sent to
+    Webhook = "YOUR WEBHOOK HERE", -- The discord webhook where the logs will be sent to
 }
